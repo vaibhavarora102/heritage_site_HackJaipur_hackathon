3087e83e3166d1581166ef11d444ae1ac9ce8b91
ghackg11 <56071613+ghackg11@users.noreply.github.com>
2020-06-20 14:37:09 +0530

    Add files via upload

---
+++ map work/mapdata.js
@@ -0,0 +1,235 @@
+var simplemaps_countrymap_mapdata = {
+    main_settings: {
+        //General settings
+        width: "700", //'700' or 'responsive'
+        background_color: "#FFFFFF",
+        background_transparent: "yes",
+        border_color: "#ffffff",
+
+        //State defaults
+        state_description: "State description",
+        state_color: "#393e46",
+        state_hover_color: "#e3e3e3",
+        state_url: "",
+        border_size: 1.5,
+        all_states_inactive: "no",
+        all_states_zoomable: "yes",
+
+        //Location defaults
+        location_description: "Location description",
+        location_color: "#FF0067",
+        location_opacity: 0.8,
+        location_hover_opacity: 1,
+        location_url: "",
+        location_size: 25,
+        location_type: "square",
+        location_image_source: "frog.png",
+        location_border_color: "#FFFFFF",
+        location_border: 2,
+        location_hover_border: 2.5,
+        all_locations_inactive: "no",
+        all_locations_hidden: "no",
+
+        //Label defaults
+        label_color: "#d5ddec",
+        label_hover_color: "#d5ddec",
+        label_size: 22,
+        label_font: "Arial",
+        hide_labels: "no",
+        hide_eastern_labels: "no",
+
+        //Zoom settings
+        zoom: "yes",
+        back_image: "no",
+        initial_back: "no",
+        initial_zoom: "-1",
+        initial_zoom_solo: "no",
+        region_opacity: 1,
+        region_hover_opacity: 0.6,
+        zoom_out_incrementally: "yes",
+        zoom_percentage: 0.99,
+        zoom_time: 0.5,
+
+        //Popup settings
+        popup_color: "white",
+        popup_opacity: 0.9,
+        popup_shadow: 1,
+        popup_corners: 5,
+        popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+        popup_nocss: "no",
+
+        //Advanced settings
+        div: "map",
+        auto_load: "yes",
+        url_new_tab: "yes",
+        images_directory: "default",
+        fade_time: 0.1,
+        link_text: "View Website",
+        popups: "detect",
+        state_image_url: "",
+        state_image_position: "",
+        location_image_url: "",
+        manual_zoom: "yes"
+    },
+    state_specific: {
+        "1": {
+            name: "Andaman and Nicobar",
+            description: "<img src='https://www.linkpicture.com/q/andaman.jpg' type='image' style=\"width: 100% ; height: 100%; border-radius:5px; margin-bottom:10px;\">\n<p>The large and beautiful <b>shells</b> found in abundance in the Andaman and Nicobar Islands. are cleaned and polished before being used to make ashtrays, small boxes, bangles, table lamps, and jewellery. An interesting and beautiful resource is the tortoise shell which is often inlaid with silver, and used to make jewellery and decorative artefacts such as rings and crosses to be worn around the neck. Shell, because of its light colour and luminous properties, is also being used for inlay work.<p>",
+            color: "#35d0ba"
+        },
+        "2": {
+            name: "Andhra Pradesh",
+            description: "<img src='https://www.linkpicture.com/q/andhra.jpeg' type='image' style=\"width:100%; height:100%; border-radius:5px; margin-bottom:10px;\"><p>Out of the many beautiful forms of handicrafts, the <b>Kalamkari</b> in Andhra Pradesh has always been favorite with the art and craft lovers. The Kalamkari craft of Andhra Pradesh involves the art of printing and painting of fabrics. This is a rare kind of art, which uses a Kalam or quill, from which it has earned its name and also vegetable dyes.</p>",
+            color: "#35d0ba"
+        },
+        "3": {
+            name: "Arunachal Pradesh",
+            description: "<img src='https://www.linkpicture.com/q/arunanchal.jpg' type='image' style=\"width:100%; height:100%; border-radius:5px; margin-bottom:10px;\"><p>The <b>Carpets</b> of Arunachal Pradesh with attractive colours and designs are in great demand by the tourists. Monpas, the Buddhist community is famous for making perfect carpets.</p>",
+            color: "#ffcd3c"
+        },
+        "4": {
+            name: "Assam",
+            description: "<img src='https://www.linkpicture.com/q/assam.jpg' type='image' style = \"width:100%; height: 100%; border-radius:5px; margin-bottom:10px;\"><p>Assam is renowned for the production of some wonderful <b>silk</b>. The state produces superior quality silk called ‘Pat’ and ‘Muga’.  Cane and bamboo are also used here exquisitely to make beautiful handicrafts. In addition, terracotta products of Assam are also good examples of beautiful crafts.</p>",
+            color: "#ffcd3c"
+        },
+        "5": {
+            name: "Bihar",
+            description: "<img src='https://www.linkpicture.com/q/bihar.jpg' type='image' style=\"width:100%; height:100%; border-radius:5px; margin-bottom:10px;\"><p><b>Madhubani Paintings</b> in Bihar is a craft practiced in Mithilanchal. This is a creative folk painting done by women in the traditional way. These paintings portray symbolic motifs, and religious scenes. For paintings natural colors are used, which range from red, yellow, green, ochre, brown and black. Originally, these painting were done by women on the walls of their houses</p>",
+            color: "#ffcd3c"
+        },
+        "6": {},
+        "7": {
+            name: "Chhattisgarh",
+            description: "<img src='https://www.linkpicture.com/q/chhatisgarh.jpg' type='image' style=\"width:100%;height:100%; border-radius:5px; margin-bottom:10px;\"> <p>\n<b>Cotton Fabrics</b> are one of the famous and attractive handicrafts made by the tribals of Bastar. These are made of Kosa thread which is made from a kind of worm found in the forest, hand woven and hand printed by tribes who trace their lineage to the 14th century weaver-saint-poet Kabir. </p>",
+            color: "#ffcd3c"
+        },
+        "8": {},
+        "9": {},
+        "10": {
+            name: "Delhi",
+            description: "<img src='https://www.linkpicture.com/q/delhi.jpg' type='image' style=\"width:100%;height:100%; border-radius:5px; margin-bottom:10px;\"> <p>Delhi is famous for its local <b>silversmiths</b>, who expertise in silver art, like silver tea set, silver jewellery and showpieces. </p>",
+            color: "#d92027"
+        },
+        "11": {
+            name: "Goa",
+            description: "<img src='https://www.linkpicture.com/q/goa.jpeg' type='image' style=\"width:100%;height:100%; border-radius:5px; margin-bottom:10px;\"><p>One of the arts that continues to lend character to Goan homes and in which each art piece is known for its artistic finish is <b>terracotta pottery</b>. It includes items such as pen holders, decorative flower pots, floral designs, ashtrays and figurines of saints and gods and it is also one of the earliest arts known to mankind.</p>",
+            color: "#35d0ba"
+        },
+        "12": {
+            name: "Gujarat",
+            description: "<img src='https://www.linkpicture.com/q/guju.jpeg' type='image' style=\"width:100%; height:100%; border-radius:5px; margin-bottom:10px;\"><p><b>Zari embroidery</b> is one of the oldest handicraft forms of Gujarat. Historians say that the tradition of the Zari was started during the Mughal era. Zari was the favourite fabric of Mughals and artisans used to decorate it further with motifs and patterns resonating the Mughal glory.</p>",
+            color: "#ff9234"
+        },
+        "13": {
+            name: "Haryana",
+            description: "<img src='https://www.linkpicture.com/q/haryana.jpg' type='image' style=\"width:100%;height:100%; border-radius:5px; margin-bottom:10px;\"><p><b>Durries</b> are rugs having spectacular geometric designs. Durries are usually made on blue colored cloth with white triangle designs and Panipat is the hub of making durries in Haryana.</p>",
+            color: "#d92027"
+        },
+        "14": {
+            name: "Himachal Pradesh",
+            description: "<img src='https://www.linkpicture.com/q/himachal.jpeg' type='image' style=\"width:100%;height:100%; border-radius:5px; margin-bottom:10px;\"><p><b>Thangkas</b> are brightly coloured cloth paintings, which are mostly used as ritual paintings exhibited during some Buddhist festivals. International tourists love these paintings. They generally depict lord Buddha and other deities as well as the wheel of life.</p>",
+            color: "#d92027"
+        },
+        "16": {
+            name: "Jharkhand",
+            description: "<img src='https://www.linkpicture.com/q/jharkhand.jpg' type='image' style=\"width:100%;height:100%; border-radius:5px; margin-bottom:10px;\"><p>Jharkhand <b>ornaments</b> are one of the major forms of crafts in the state of Jharkhand in India. The ornaments created by the people belonging to the tribal groups of Jharkhand show a great deal of perfection in the art.The major types of jewelry that they wear are neck pieces, earrings and ornaments worn on the wrists and other parts of the arm. \n</p>",
+            color: "#ffcd3c"
+        },
+        "17": {
+            name: "Karnataka",
+            description: "<img src='https://www.linkpicture.com/q/karnataka.jpg' type='image' style=\"width:100%;height:100%; border-radius:5px; margin-bottom:10px;\"><p>The very mention of Mysore spells the fragrance of <b>sandalwood</b>. This soft material is used extensively to produce charming art pieces. The range of objects and designs are varied and the gudigar families of Shimoga, Uttara Kannada and Mysore districts specialize in this craft. Sandalwood lends itself to extremely delicate carving that is needed to embellish the figures of gods and goddesses.</p>",
+            color: "#35d0ba"
+        },
+        "18": {
+            name: "Kerala",
+            description: "<img src='https://www.linkpicture.com/q/kerela.jpg' type='image' style=\"width:100%;height:100%; border-radius:5px; margin-bottom:10px;\"><p><b>Coconut</b> which is omnipresent in Kerala is used in making numerous Handicrafts. While the Coir is used to make Baskets, Fans, bags, rugs, and a lot many products and the coconut shell is used to make products like bowls, vases, animal shaped toys, and even Hookahs. You can find these items easily in Kollam and Calicut.</p>",
+            color: "#35d0ba"
+        },
+        "19": {
+            name: "Lakshadweep",
+            color: "#35d0ba"
+        },
+        "20": {
+            name: "Madhya Pradesh",
+            description: "<img src='https://www.linkpicture.com/q/madhya.jpg' type='image' style=\"width:100%;height:100%;  border-radius:5px; margin-bottom:10px;\"><p>The <b>bamboo work</b> of Madhya Pradesh is a reflection of its raw rural lifestyle, and continues to serve purposes both utilitarian as well as ornamental. Abundantly available in the regions of Shahdol, Balaghat, Mandla and Seoni, Bamboo is used to create products ranging from hunting tools to household baskets. The Baiga, Gond, and Korku tribal communities of MP are known to be the best in this craft, fusing ancestral expertise with contemporary designs.</p>",
+            color: "#ff9234"
+        },
+        "21": {
+            name: "Maharashtra",
+            description: "<img src='maharahstra.jpg' type='image' style=\"width:100%;height:100%;  border-radius:5px; margin-bottom:10px;\"><p><b>Kolhapuri chappal</b> making is a major handicraft industry that employees over 20,000 craftspersons in the district. Kolhapuri chappals are flat, intricately patterned, handcrafted leather footwear traditionally made in Kolhapur by the community whose hereditary occupation is tanning and leather work.  </p>",
+            color: "#ff9234"
+        },
+        "22": {
+            name: "Manipur",
+            description: "<img src='manipur.jpg' type='image' style=\"width:100%;height:100%;  border-radius:5px; margin-bottom:10px;\"><p><b>Kauna</b> is a kind of reed that mostly grows in wetlands and marshes. In Manipur, this is used for making mats, and cushions. Numerous craftsmen in Manipur are engaged in this particular craft. Kauna Crafts creates products that are used for domestic use and export market.</p>",
+            color: "#ffcd3c"
+        },
+        "23": {
+            name: "Meghalaya",
+            description: "<img src='meghalaya.jpg' type='image' style=\"width:100%;height:100%;  border-radius:5px; margin-bottom:10px;\"><p>Prominent work here includes <b>woodcarving</b> and <b>bamboo work</b> because the raw material for these products is found in abundance in the state. The state produces some amazing cane mats, winnowing fans, caps, umbrellas, stools and baskets.</p>",
+            color: "#ffcd3c"
+        },
+        "24": {
+            name: "Mizoram",
+            description: "<img src='mizoram.jpg' type='image' style=\"width:100%;height:100%;  border-radius:5px; margin-bottom:10px;\"><p><b>Weaving</b> is a popular craft in Mizoram and the artisans of the state are experts in this craft form. They are proficient in changing the simple raw material in beautiful designs and patterns and produce lovely fabric. The Intricate traditional designs are woven by the Mizo women on their looms.</p>",
+            color: "#ffcd3c"
+        },
+        "25": {
+            name: "Nagaland",
+            description: "<img src='nagaland.jpeg' type='image' style=\"width:100%;height:100%;  border-radius:5px; margin-bottom:10px;\"><p><b>Blacksmithy</b> or metal works, though a comparatively recent craft, is more popular. The smiths produce the normal requirements of villagers like the Dao, axe, sickle, knives, spear points and butts etc. The Konyaks are the best blacksmiths. They make muzzle-loading guns as well. The Lothas regard this trade unlucky and believe that no blacksmith lives long after he stops the work. </p>",
+            color: "#ffcd3c"
+        },
+        "26": {
+            name: "Orissa",
+            description: "<img src='orrisa.jpg' type='image' style=\"width:100%;height:100%;  border-radius:5px; margin-bottom:10px;\"><p>Believed to be originated in the early 12th-century, Pattachitra is an icon painting which makes it one of the most popular living art forms in India. People in Odisha can be seen practising it daily. </p>",
+            color: "#ffcd3c"
+        },
+        "27": {
+            name: "Puducherry",
+            color: "#35d0ba"
+        },
+        "28": {
+            name: "Punjab",
+            color: "#d92027"
+        },
+        "29": {
+            name: "Rajasthan",
+            color: "#ff9234"
+        },
+        "30": {
+            name: "Sikkim",
+            color: "#ffcd3c"
+        },
+        "31": {
+            name: "Tamil Nadu",
+            color: "#35d0ba"
+        },
+        "32": {
+            name: "Tripura",
+            color: "#ffcd3c"
+        },
+        "33": {
+            name: "Uttar Pradesh",
+            color: "#d92027"
+        },
+        "34": {
+            name: "Uttaranchal",
+            color: "#d92027"
+        },
+        "35": {
+            name: "West Bengal",
+            color: "#ffcd3c"
+        },
+        "36": {
+            name: "Jammu and Kashmir",
+            color: "#d92027"
+        },
+        "37": {
+            name: "Telangana",
+            color: "#35d0ba"
+        }
+    },
+    locations: {},
+    labels: {},
+    regions: {}
+};
